5ef7b8774bec62f0b78099406b7d5714d3004e3c
LuisHenaoS <luiismiiguelhenaosaldarriiaga@gmail.com>
2024-12-16 10:46:52 +0100

    tests con nuevos cambios

--- deploy/deploy_production.sh
+++ deploy/deploy_production.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "🚀 Desplegando en Producción..."
+echo " Desplegando en Producción..."
 docker pull proyecto/imagen:${GIT_COMMIT}
 docker stop production-container || true
 docker rm production-container || true
 docker run -d --name production-container -p 8080:80 proyecto/imagen:${GIT_COMMIT}
-echo "✅ Despliegue en Producción completado."
+echo "Despliegue en Producción completado."
